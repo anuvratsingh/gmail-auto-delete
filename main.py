af8145c76ef1bccc6fbd54e509e35c277caec9df
Anuvrat <anuvrats06@gmail.com>
2020-10-29 02:42:21 +0530

    formatted

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import pyautogui
 import time
 
-try: 
-  while True:
-    # DELETE
+try:
+    while True:
+      # DELETE
 
-    # Select all
-    # pyautogui.click(x=?, y=?)
+      # Select all
+      # pyautogui.click(x=?, y=?)
 
-    # Delete
-    # pyautogui.click(x=?, y=?)
-    # time.sleep(2) 
-    
+      # Delete
+      # pyautogui.click(x=?, y=?)
+      # time.sleep(2)
 
-    # FOR COORDINATES
-    # x,y = pyautogui.position()
-    # position_str = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
-    # print(position_str, flush=True)
-    # # print("\b" * len(position_str), end="", flush=True)
-    # time.sleep(1)
+      # FOR COORDINATES
+      # x,y = pyautogui.position()
+      # position_str = "X: " + str(x).rjust(4) + " Y: " + str(y).rjust(4)
+      # print(position_str, flush=True)
+      # # print("\b" * len(position_str), end="", flush=True)
+      # time.sleep(1)
 
 except KeyboardInterrupt:
-  print("\n")
+    print("\n")
